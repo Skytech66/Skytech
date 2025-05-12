da388dce31dad8d2992543f1b1073a0e88ea07d3
Skytech66 <dadzieitservices123@gmail.com>
2025-05-12 11:36:39 +0000

    header.php

--- www/teachers/header.php
+++ www/teachers/header.php
@@ -17,9 +17,7 @@ $conn = db_conn();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css" rel="stylesheet" />
@@ -32,107 +30,139 @@ $conn = db_conn();
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="../include/js/functions.js"></script>
-
-    <style>
-        .text-center {
-            font-size: 1.5rem; /* Adjust font size */
-            font-weight: bold; /* Make it bold */
-            color: #333; /* Change color to a darker shade */
-            margin-top: 10px; /* Add some space above */
-        }
-    </style>
 </head>
 <body>
 
 <div class="wrapper d-flex align-items-stretch">
     <nav id="sidebar">
         <div class="p-4 pt-5">
-            <a href="#" class="img logo rounded-circle mb-3" style="background-image: url(../images/logo.jpg);"></a>
-            <h4 class="text-center">EduPro Suite 2.0</h4> <!-- Title positioned directly under the logo -->
+            <a href="#" class="img logo rounded-circle mb-5" style="background-image: 
+                <a href="#" class="circle mb-5" style="background-image: url('/www/images/icon.png');"></a>
+                        <h4 class="text-center">EduPro Suite 2.0</h4> <!-- Title positioned directly under the logo -->
+
+
             <ul class="list-unstyled components mb-5">
                 <li class="active">
-                    <a href="index.php?dashboard">Dashboard</a>
+                    <a href="index.php?dashboard"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
                 </li>
                 <li>
-                    <a href="index.php?exams">Exams</a>
+                    <a href="index.php?exams"><i class="fas fa-file-alt"></i> Exams</a>
                 </li>
                 <li>
-                    <a href="index.php?subjects">Subjects</a>
+                    <a href="exam_scores.php?exam_scores"><i class="fas fa-chart-line"></i> Exam Scores</a>
                 </li>
                 <li>
-                    <a href="index.php?lesson_notes">Lesson notes</a>
+                    <a href="index.php?lesson_notes"><i class="fas fa-book"></i> Lesson Notes</a>
                 </li>
                 <li>
-                    <a href="index.php?login">Attendance register</a>
+                    <a href="index.php?subjects"><i class="fas fa-book-open"></i> Subjects</a>
                 </li>
                 <li>
-                    <a href="form.php">Add students</a>
+                    <a href="index.php?form"><i class="fas fa-user-plus"></i> Add Students</a>
                 </li>
                 <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Contact</a>
+                    <a href="index.php?view_students"><i class="fas fa-eye"></i> View Students & Add Passport Photo</a>
+                </li>
+                <li>
+				   <a href="index.php?login"><i class="fas fa-user-check"></i> Attendance Register</a>
+                </li>
+                <li>
+                    <a href="index.php?mails"><i class="fas fa-envelope"></i> Emails</a>
+                </li>
+                <li>
+                    
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-file-invoice"></i> Generate Reports</a>
                     <ul class="collapse list-unstyled" id="pageSubmenu">
                         <li>
-                            <a href="index.php?sms">>SMS</a>
-                        </li>
-                        <li>
-                            <a href="index.php?mails">>Emails</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Generate Reports</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                                                        <a href="#Generate_Report_Cards" data-toggle="modal" data-target="#Generate_Report_Cards">>Students Report</a>
+                            <a href="#Generate_Report_Cards" data-toggle="modal" data-target="#Generate_Report_Cards"><i class="fas fa-file-alt"></i> Students Report</a>
                         </li>
                     </ul>
                 </li>
             </ul>
-        </div>
-    </nav>
 
-    <div id="content" class="p-4 p-md-5 pt-5">
-        <h2 class="mb-4">Welcome to EduPro Suite</h2>
-        <p>Your content goes here...</p>
-        <!-- Add your main content here -->
-    </div>
+            <div class="footer">
+                <p>&copy;<script>document.write(new Date().getFullYear());</script> powered <i class="icon-heart" aria-hidden="true"></i> by <a href="https://me.co.ke" target="_blank">Swipeware tech.</a></p>
+            </div>
+		    </nav>
+
+   <!-- Page Content  -->
+   <div id="content" class="content-container">
+  <div class="container-fluid">
+    <nav class="navbar">      
+	<button type="button" id="sidebarCollapse" class="btn btn-primary">
+	  <i class="fa fa-bars"></i>
+	  <span class="sr-only">Toggle Menu</span>
+	</button>
+	<button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<i class="fa fa-bars"></i>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	  <ul class="nav navbar-nav ml-auto">
+		<li class="nav-item active">
+			<a class="nav-link" href="#Change_Password"  data-toggle="modal" data-target="#Change_Password">Change password</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="../include/functions.php?logout=1">Logout</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Help</a>
+		</li>
+	  </ul>
+	</div>
+</nav>
 </div>
 
-<!-- Modal for Generate Report Cards -->
-<div class="modal fade" id="Generate_Report_Cards" tabindex="-1" role="dialog" aria-labelledby="Generate_Report_Cards_Label" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="Generate_Report_Cards_Label">Generate Report Cards</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- Form for generating report cards -->
-                <form>
-                    <div class="form-group">
-                        <label for="student_id">Student ID</label>
-                        <input type="text" class="form-control" id="student_id" placeholder="Enter Student ID">
-                    </div>
-                    <div class="form-group">
-                        <label for="term">Select Term</label>
-                        <select class="form-control" id="term">
-                            <option>Term 1</option>
-                            <option>Term 2</option>
-                            <option>Term 3</option>
-                        </select>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Generate</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+            <div class="modal fade" id="Generate_Report_Cards" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header bg-warning text-light">
+                            <h5 class="modal-title" id="exampleModalLabel">SEARCH MARK SHEET</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
 
-<script>
-    // Optional: Add any custom JavaScript here
-</script>
+                        <div class="modal-body">
+                            <form action="report_cards.php" method="POST" enctype="multipart/form-data">
+                                <div class="row">
+                                    <div class="col">
+                                        <label for="recipient-name" class="col-form-label">Select Class:</label>
+                                        <div class="form-group">
+                                            <select class="form-select form-select-sm" aria-label=".form-select-md example" id="askclass" name="askclass" required>
+                                                <option selected value="">Select class</option>
+                                                <?php 
+                                                    $sql = "SELECT * from class ORDER BY classid DESC";
+                                                    $res = $conn->query($sql);
+                                                    while($row = $res->fetchArray(SQLITE3_ASSOC)){
+                                                        $id = $row["classname"];
+                                                ?>
+                                                    <option value="<?php echo $row["classname"]; ?>"> <?php echo $row["classname"]; ?> </option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+                                    </div>
 
-</body>
-</html>
+                                    <div class="col">
+                                        <label for="recipient-name" class="col-form-label">Select Exam:</label>
+                                        <div class="form-group">
+                                            <select class="form-select form-select-sm" aria-label=".form-select-md example" id="exam" name="exam" required>
+                                                <option selected value="">Select exam</option>
+                                                <?php 
+                                                    $res = "SELECT * from exam ORDER BY examid DESC";
+                                                    $ret1 = $conn->query($res);
+                                                    while($row = $ret1->fetchArray(SQLITE3_ASSOC)) {
+                                                ?>
+                                                    <option value="<?php echo $row["examname"]; ?>"> <?php echo $row["examname"]; ?> </option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+                                    </div>  
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" name="submit" value="search" class="btn btn-primary">Generate Report Cards</button>
+                                     </div>
+                               </form>   
+                             </div>
+                         </div></div>
+                       </div>
